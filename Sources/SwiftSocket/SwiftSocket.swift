029563da9b0cae1b9192429fafa0283270d48722
Levi Dhuyvetter <levidhuyvetter@icloud.com>
2022-12-15 00:03:02 +0000

    Initial Commit

---
+++ Sources/SwiftSocket/SwiftSocket.swift
@@ -0,0 +1,6 @@
+public struct SwiftSocket {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftSocketTests/SwiftSocketTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftSocket
+
+final class SwiftSocketTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftSocket().text, "Hello, World!")
+    }
+}
